bbe728e1d4a39e6f5d9b8414fa3d130f126ae83e
dakson99 <darko99.mikavica@gmail.com>
2024-08-18 13:24:20 +0200

    Add weather search functionality and dynamic image update based on weather conditions

---
+++ script.js
@@ -0,0 +1,49 @@
+const container = document.querySelector(".container"),
+    search = document.querySelector(".search-box button"),
+    weatherBox = document.querySelector(".weather-box"),
+    weatherDetails = document.querySelector(".weather-details");
+
+search.addEventListener("click", () => {
+    const APIKey = 'b8921f3aaeb18550bd014acfdd85ca97';
+    const city = document.querySelector(".search-box input").value;
+
+    if (city == '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+        const image = document.querySelector(".weather-box img");
+        const temperature = document.querySelector(".weather-box temperature");
+        const description = document.querySelector(".weather-box description");
+        const humidity = document.querySelector(".weather-details .humidity span");
+        const wind = document.querySelector(".weather-details .wind span");
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'images/cloud.png';
+                break;
+
+            case 'Mist':
+                image.src = 'images/mist.png';
+                break;
+
+            case 'Haze':
+                image.src = 'images/mist.png';
+                break;
+
+            default:
+                image.src = 'images/cloud.png';
+        }
+    });
+});
