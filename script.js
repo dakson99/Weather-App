c18f16b592159d1eb208808c61b8150fbbbfaeac
dakson99 <darko99.mikavica@gmail.com>
2024-08-19 16:23:52 +0200

    Add dynamic weather data display including temperature, description, humidity, and wind speed

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ search.addEventListener("click", () => {
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
         const image = document.querySelector(".weather-box img");
-        const temperature = document.querySelector(".weather-box temperature");
-        const description = document.querySelector(".weather-box description");
+        const temperature = document.querySelector(".weather-box .temperature");
+        const description = document.querySelector(".weather-box .description");
         const humidity = document.querySelector(".weather-details .humidity span");
         const wind = document.querySelector(".weather-details .wind span");
 
@@ -45,5 +45,10 @@ search.addEventListener("click", () => {
             default:
                 image.src = 'images/cloud.png';
         }
+
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
     });
 });
