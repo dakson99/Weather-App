dcb8b03b934b43e6d65066efea75664bd4d01c28
dakson99 <darko99.mikavica@gmail.com>
2024-08-21 15:24:44 +0200

    Add overflow hidden for elements, and add city-hide element and enhance city comparison logic

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const container = document.querySelector(".container"),
     search = document.querySelector(".search-box button"),
     weatherBox = document.querySelector(".weather-box"),
     weatherDetails = document.querySelector(".weather-details"),
-    error404 = document.querySelector(".not-found");
+    error404 = document.querySelector(".not-found"),
+    cityHide = document.querySelector(".city-hide");
 
 search.addEventListener("click", () => {
     const APIKey = 'b8921f3aaeb18550bd014acfdd85ca97';
@@ -14,6 +15,7 @@ search.addEventListener("click", () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
 
         if (json.cod == '404') {
+            cityHide.textContent = city;
             container.style.height = '400px';
             weatherBox.classList.remove('active');
             weatherDetails.classList.remove('active');
@@ -21,10 +23,52 @@ search.addEventListener("click", () => {
             return;
         }
 
-        container.style.height = '555px';
-        weatherBox.classList.add('active');
-        weatherDetails.classList.add('active');
-        error404.classList.remove('active');
+
+
+        if (cityHide.textContent == city) {
+            return;
+        } else {
+            cityHide.textContent = city;
+
+            container.style.height = '555px';
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            error404.classList.remove('active');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+
+                case 'Mist':
+                    image.src = 'images/mist.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+
+                default:
+                    image.src = 'images/cloud.png';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+        }
 
         const image = document.querySelector(".weather-box img");
         const temperature = document.querySelector(".weather-box .temperature");
@@ -32,38 +76,6 @@ search.addEventListener("click", () => {
         const humidity = document.querySelector(".weather-details .humidity span");
         const wind = document.querySelector(".weather-details .wind span");
 
-        switch (json.weather[0].main) {
-            case 'Clear':
-                image.src = 'images/clear.png';
-                break;
 
-            case 'Rain':
-                image.src = 'images/rain.png';
-                break;
-
-            case 'Snow':
-                image.src = 'images/snow.png';
-                break;
-
-            case 'Clouds':
-                image.src = 'images/cloud.png';
-                break;
-
-            case 'Mist':
-                image.src = 'images/mist.png';
-                break;
-
-            case 'Haze':
-                image.src = 'images/mist.png';
-                break;
-
-            default:
-                image.src = 'images/cloud.png';
-        }
-
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-        description.innerHTML = `${json.weather[0].description}`;
-        humidity.innerHTML = `${json.main.humidity}%`;
-        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
     });
 });
